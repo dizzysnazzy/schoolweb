6ea0c009fbabf78e8810032fdc786379e79646e3
dizzysnazzy <dismaskipchumba2@gmail.com>
2023-06-24 11:15:28 +0000

    some few changes

--- schoolsite/schoolsite/settings.py
+++ schoolsite/schoolsite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-)yhei+dq0%_@=u=+c&jhgnsl+q9+cf_)e%ubeb%l)!h764+0j3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dizzysnazzy.pythonanywhere.com']
 
 
 # Application definition
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'schoolweb/schoolsite/static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     #os.path.join(BASE_DIR, 'bets', 'static'),
